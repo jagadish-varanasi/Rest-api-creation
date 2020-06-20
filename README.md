913dfa3142042bfb42fd6b5e8eb6613bb9efb863
Jagadish varanasi <jagadishvaranasi123@gmail.com>
2020-06-20 17:12:25 +0530

    error handling

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Initialize with npm init
+
+## Install express
+
+## Define server.js file (PORT!!)
+
+## optional!! to avoid restarting server again when changes made install nodemon
+
+## whenever want to start server use nodemon server.js instead node server.js! 
+### by editing package.json file  
+### "start":nodemon server.js    so that u can use npm start to start server and it restsrts whenever changes are made
+
+### install morgan for error handling and visibility in propmt
--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express();
+const morgan=require('morgan')
 
 const productRoutes = require('./api/routes/product')
 const orderRoutes = require('./api/routes/order');
 
+app.use(morgan('dev'))
 
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//If u reach here it means that there is a error
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
